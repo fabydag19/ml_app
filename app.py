ef836a9be515a2c64ac4e320dc2bd68c6fbe5b42
fabydag19 <fabydag19@gmail.com>
2024-08-08 17:45:38 +0200

    Form first version

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/model', methods=['GET', 'POST'])
+def home():
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
